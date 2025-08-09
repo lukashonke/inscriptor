984ba29edacbf7889f52cc473e55936832cbc3ac
Lukas Honke <honke.lukas@gmail.com>
2025-08-09 15:02:27 +0200

    Added trash bin

--- src/components/Common/Files/FileListRowContextMenu.vue
+++ src/components/Common/Files/FileListRowContextMenu.vue
@@ -1,172 +1,200 @@
 <template>
   <q-list dense style="min-width: 100px">
-    <q-item v-if="fileStore.checkCanHaveChildren(file)" clickable v-close-popup @click.prevent="addFile(file, $event)">
-      <q-item-section avatar>
-        <q-icon name="mdi-plus"/>
-      </q-item-section>
-      <q-item-section>Add</q-item-section>
-    </q-item>
+    <!-- Files in trash bin: Show only restore and permanent delete -->
+    <template v-if="isInTrashBin">
+      <q-item clickable v-close-popup @click="restoreFile">
+        <q-item-section avatar>
+          <q-icon name="mdi-restore" color="positive"/>
+        </q-item-section>
+        <q-item-section class="text-positive">Restore</q-item-section>
+      </q-item>
+      <q-item clickable v-close-popup @click="permanentlyDeleteFile">
+        <q-item-section avatar>
+          <q-icon name="mdi-delete-forever" color="negative"/>
+        </q-item-section>
+        <q-item-section class="text-negative">Delete Permanently</q-item-section>
+      </q-item>
+    </template>
 
-    <q-separator />
+    <!-- Trash bin specific: Show only empty trash option -->
+    <template v-else-if="file.isTrashBin">
+      <q-item clickable v-close-popup @click="emptyTrash" :disable="trashIsEmpty">
+        <q-item-section avatar>
+          <q-icon name="mdi-delete-empty" color="negative"/>
+        </q-item-section>
+        <q-item-section class="text-negative">Empty Trash</q-item-section>
+      </q-item>
+    </template>
 
-    <q-item clickable v-close-popup @click.prevent="renameFile(file, $event)">
-      <q-item-section avatar>
-        <q-icon name="mdi-pencil-outline"/>
-      </q-item-section>
-      <q-item-section>Rename</q-item-section>
-    </q-item>
+    <!-- Regular files (not in trash, not trash bin): Show all operations -->
+    <template v-else>
+      <q-item v-if="fileStore.checkCanHaveChildren(file)" clickable v-close-popup @click.prevent="addFile(file, $event)">
+        <q-item-section avatar>
+          <q-icon name="mdi-plus"/>
+        </q-item-section>
+        <q-item-section>Add</q-item-section>
+      </q-item>
 
-    <q-item clickable v-if="fileTemplates?.length > 0 && fileStore.checkCanHaveChildren(file)">
-      <q-item-section avatar>
-        <q-icon name="mdi-plus"/>
-      </q-item-section>
+      <q-separator />
 
-      <q-item-section>Add from template</q-item-section>
-      <q-item-section side>
-        <q-icon name="keyboard_arrow_right"/>
-      </q-item-section>
+      <q-item clickable v-close-popup @click.prevent="renameFile(file, $event)">
+        <q-item-section avatar>
+          <q-icon name="mdi-pencil-outline"/>
+        </q-item-section>
+        <q-item-section>Rename</q-item-section>
+      </q-item>
 
-      <q-menu anchor="top end" self="top start">
-        <q-list dense>
-          <q-item clickable v-close-popup @click="addTemplate(file, template)" v-for="template in fileTemplates"
-                  :key="template.id">
-            <q-item-section>
-              <q-item-label>{{ file.title }}</q-item-label>
-            </q-item-section>
-            <q-item-section side>
-              <q-icon :name="file.icon"/>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-menu>
+      <q-item clickable v-if="fileTemplates?.length > 0 && fileStore.checkCanHaveChildren(file)">
+        <q-item-section avatar>
+          <q-icon name="mdi-plus"/>
+        </q-item-section>
 
-    </q-item>
+        <q-item-section>Add from template</q-item-section>
+        <q-item-section side>
+          <q-icon name="keyboard_arrow_right"/>
+        </q-item-section>
 
-    <q-item clickable>
-      <q-item-section avatar>
-        <q-icon name="mdi-flag-outline"/>
-      </q-item-section>
+        <q-menu anchor="top end" self="top start">
+          <q-list dense>
+            <q-item clickable v-close-popup @click="addTemplate(file, template)" v-for="template in fileTemplates"
+                    :key="template.id">
+              <q-item-section>
+                <q-item-label>{{ file.title }}</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-icon :name="file.icon"/>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
 
-      <q-item-section>State</q-item-section>
-      <q-item-section side>
-        <q-icon name="keyboard_arrow_right"/>
-      </q-item-section>
+      </q-item>
 
-      <q-menu anchor="top end" self="top start">
-        <q-list dense>
-          <q-item clickable v-close-popup @click="fileStore.setFileState(file, null)" >
-            <q-item-section side>
-              <q-icon name="mdi-close" color="transparent"/>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Clear</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item clickable v-close-popup @click="fileStore.setFileState(file, label)" v-for="label in promptStore.statuses" :key="label.label" :active="file.state?.label === label.label" active-class="text-weight-bold">
-            <q-item-section side>
-              <q-icon name="mdi-flag-outline" :color="label.color"/>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ label.label }}</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-separator />
-          <q-item clickable v-close-popup @click="layoutStore.openConfiguration('statuses')" >
-            <q-item-section side>
-              <q-icon name="mdi-playlist-edit" color="primary"/>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Edit statuses...</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-menu>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-icon name="mdi-flag-outline"/>
+        </q-item-section>
 
-    </q-item>
+        <q-item-section>State</q-item-section>
+        <q-item-section side>
+          <q-icon name="keyboard_arrow_right"/>
+        </q-item-section>
 
-    <q-item clickable>
-      <q-item-section avatar>
-        <q-icon name="mdi-tag-outline"/>
-      </q-item-section>
+        <q-menu anchor="top end" self="top start">
+          <q-list dense>
+            <q-item clickable v-close-popup @click="fileStore.setFileState(file, null)" >
+              <q-item-section side>
+                <q-icon name="mdi-close" color="transparent"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Clear</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="fileStore.setFileState(file, label)" v-for="label in promptStore.statuses" :key="label.label" :active="file.state?.label === label.label" active-class="text-weight-bold">
+              <q-item-section side>
+                <q-icon name="mdi-flag-outline" :color="label.color"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ label.label }}</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable v-close-popup @click="layoutStore.openConfiguration('statuses')" >
+              <q-item-section side>
+                <q-icon name="mdi-playlist-edit" color="primary"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Edit statuses...</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
 
-      <q-item-section>Label</q-item-section>
-      <q-item-section side>
-        <q-icon name="keyboard_arrow_right"/>
-      </q-item-section>
+      </q-item>
 
-      <q-menu anchor="top end" self="top start">
-        <q-list dense>
-          <q-item clickable v-close-popup @click="fileStore.clearFileLabels(file)" >
-            <q-item-section side>
-              <q-icon name="mdi-close" color="transparent"/>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Clear</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item clickable v-close-popup @click="fileStore.toggleFileLabel(file, label)" v-for="label in promptStore.labels" :key="label.label" :active="fileStore.hasFileLabel(file, label)" active-class="text-weight-bold">
-            <q-item-section side>
-              <q-badge :color="label.color"/>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ label.label }}</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-separator />
-          <q-item clickable v-close-popup @click="layoutStore.openConfiguration('labels')" >
-            <q-item-section side>
-              <q-icon name="mdi-playlist-edit" color="primary"/>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Edit labels...</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-menu>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-icon name="mdi-tag-outline"/>
+        </q-item-section>
 
-    </q-item>
+        <q-item-section>Label</q-item-section>
+        <q-item-section side>
+          <q-icon name="keyboard_arrow_right"/>
+        </q-item-section>
 
-    <q-item clickable>
-      <q-item-section avatar>
-        <q-icon name="mdi-content-copy"/>
-      </q-item-section>
+        <q-menu anchor="top end" self="top start">
+          <q-list dense>
+            <q-item clickable v-close-popup @click="fileStore.clearFileLabels(file)" >
+              <q-item-section side>
+                <q-icon name="mdi-close" color="transparent"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Clear</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="fileStore.toggleFileLabel(file, label)" v-for="label in promptStore.labels" :key="label.label" :active="fileStore.hasFileLabel(file, label)" active-class="text-weight-bold">
+              <q-item-section side>
+                <q-badge :color="label.color"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ label.label }}</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable v-close-popup @click="layoutStore.openConfiguration('labels')" >
+              <q-item-section side>
+                <q-icon name="mdi-playlist-edit" color="primary"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Edit labels...</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
 
-      <q-item-section>Copy Settings</q-item-section>
-      <q-item-section side>
-        <q-icon name="keyboard_arrow_right"/>
-      </q-item-section>
+      </q-item>
 
-      <q-menu anchor="top end" self="top start">
-        <q-list dense>
-          <q-item clickable v-close-popup @click="applyToChildren(file)" >
-            <q-item-section side>
-              <q-icon name="mdi-content-copy"/>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Copy Settings to Children</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-menu>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-icon name="mdi-content-copy"/>
+        </q-item-section>
 
-    </q-item>
+        <q-item-section>Copy Settings</q-item-section>
+        <q-item-section side>
+          <q-icon name="keyboard_arrow_right"/>
+        </q-item-section>
 
+        <q-menu anchor="top end" self="top start">
+          <q-list dense>
+            <q-item clickable v-close-popup @click="applyToChildren(file)" >
+              <q-item-section side>
+                <q-icon name="mdi-content-copy"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Copy Settings to Children</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
 
+      </q-item>
 
-    <q-item clickable v-close-popup @click="exportFile">
-      <q-item-section avatar>
-        <q-icon name="mdi-download"/>
-      </q-item-section>
-      <q-item-section>Export...</q-item-section>
-    </q-item>
+      <q-item clickable v-close-popup @click="exportFile">
+        <q-item-section avatar>
+          <q-icon name="mdi-download"/>
+        </q-item-section>
+        <q-item-section>Export...</q-item-section>
+      </q-item>
 
-    <q-separator/>
-    <q-item clickable v-close-popup @click="deleteFile">
-      <q-item-section avatar>
-        <q-icon name="mdi-delete-outline" color="negative"/>
-      </q-item-section>
-      <q-item-section class="text-negative">Delete</q-item-section>
-    </q-item>
+      <q-separator/>
+      <!-- Regular files: Show normal delete (but not for static files) -->
+      <q-item v-if="!file.isStatic" clickable v-close-popup @click="deleteFile">
+        <q-item-section avatar>
+          <q-icon name="mdi-delete-outline" color="negative"/>
+        </q-item-section>
+        <q-item-section class="text-negative">Delete</q-item-section>
+      </q-item>
+    </template>
   </q-list>
 </template>
 <script setup>
@@ -187,11 +215,22 @@ const props = defineProps({
   },
 });
 
+// Check if file is in trash bin
+const isInTrashBin = computed(() => {
+  return props.file.parentId === '__trash_bin__' || props.file.deletedAt;
+});
+
+// Check if trash bin is empty
+const trashIsEmpty = computed(() => {
+  if (!props.file.isTrashBin) return false;
+  return !props.file.children || props.file.children.length === 0;
+});
+
 function deleteFile () {
 
   Dialog.create({
-    title: 'Delete file',
-    message: 'Are you sure you want to delete this file? It cannot be reverted.',
+    title: 'Move to Trash',
+    message: 'Are you sure you want to move this file to trash? You can restore it later from the trash bin.',
     cancel: true,
     persistent: true
   }).onOk(() => {
@@ -272,6 +311,32 @@ function countChildrenResursive(file) {
     });
   }
   return count;
+}
+
+async function restoreFile() {
+  await fileStore.restoreFile(props.file.id);
+}
+
+function permanentlyDeleteFile() {
+  Dialog.create({
+    title: 'Permanently Delete File',
+    message: 'Are you sure you want to permanently delete this file? This action cannot be undone.',
+    cancel: true,
+    persistent: true
+  }).onOk(async () => {
+    await fileStore.permanentlyDeleteFile(props.file.id);
+  });
+}
+
+function emptyTrash() {
+  Dialog.create({
+    title: 'Empty Trash',
+    message: 'Are you sure you want to permanently delete all files in the trash? This action cannot be undone.',
+    cancel: true,
+    persistent: true
+  }).onOk(async () => {
+    await fileStore.emptyTrash();
+  });
 }
 
 const fileTemplates = computed(() => promptStore.fileTemplates);
