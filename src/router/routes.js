dce7dbc426c4107b0bd34541ec5617665b107431
Lukas Honke <honke.lukas@gmail.com>
2025-10-20 16:33:36 +0200

    feat: add dynamic layout and page loading based on screen size

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,26 @@
+import { isMobileScreen } from 'src/common/utils/screenUtils';
+
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => {
+      // Dynamically choose layout based on screen width
+      if (isMobileScreen()) {
+        return import('layouts/MobileLayout.vue')
+      }
+      return import('layouts/MainLayout.vue')
+    },
     children: [
-      { path: '', component: () => import('pages/MainPage.vue') }
+      {
+        path: '',
+        component: () => {
+          // Dynamically choose page based on screen width
+          if (isMobileScreen()) {
+            return import('pages/MobilePage.vue')
+          }
+          return import('pages/MainPage.vue')
+        }
+      }
     ]
   },
 
